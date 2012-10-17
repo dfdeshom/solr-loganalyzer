561bc2be45211db6ada89742be513707f37bd4b8
Martin Filliau <martin@filliau.com>
2012-10-17 18:54:02 +0100

    Added empty tests class.

---
+++ tests.py
@@ -0,0 +1,15 @@
+import unittest
+from loganalyzer import CoreCounter
+
+
+class TestCoreCounter(unittest.TestCase):
+
+    def setUp(self):
+        self.logs = ''
+
+    def test_parse(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
