4aa08cdeb5a9f5df3211f67e6d4422b3ab0dc764
Martin Filliau <martin@filliau.com>
2012-10-17 18:51:07 +0100

    Cleaned a bit.

--- loganalyzer.py
+++ loganalyzer.py
@@ -7,6 +7,7 @@ LINE_RE = re.compile("INFO:\s+\[(?P<core>\w+)\]\s+webapp=/\w+\s+path=(?P<path>/\
 """
 lines we want:
 INFO: [arstechnicacogtree] webapp=/solr path=/mlt params={mlt.count=16&fl=link,title,author,pub_date,thumb_url_medium,metadata,section&start=0&q=link_aliases:http\://arstechnica.com/apple/news/2009/02/the\-case\-of\-the\-app\-store\-ripoff.ars&wt=json&fq=pub_date:[NOW/DAY-14DAYS+TO+NOW/DAY%2B1DAY]&rows=16} status=0 QTime=2
+INFO: [places] webapp=/solr path=/select/ params={pf=*&sort=geodist()+asc&fl=*,_dist_:geodist()&q=*&sfield=location&pt=-1.260326940083812,51.759690475011105&wt=json&spellcheck.collate=true&defType=edismax} hits=9830 status=0 QTime=11
 """
 
 class CoreCounter(object):
@@ -18,8 +19,7 @@ class CoreCounter(object):
         self.qtimes = Counter()
         
     def __repr__(self):
-        s = "<Core '%s' with %i endpoints %i search urls>" %(self.corename,len(self.endpoints), len(self.urls))
-        return s
+        return "<Core '%s' with %i endpoints %i search urls>" %(self.corename,len(self.endpoints), len(self.urls))
 
     def timestats(self):
         sqtimes = sorted(self.qtimes.values())
@@ -34,31 +34,29 @@ class CoreCounter(object):
                 ("99%", percent_99))
 
     def _pprint_topn(self,n,counter,title):
-        s = title + "\n"
-        s += "="*40 + "\n"
+        """Print the top N entries of a counter with its title.
+        """
+        s = "{0}\n{1}\n".format(title, "="*40)
         top = counter.most_common(n)
-        for index,item in enumerate(top):
+        for index, item in enumerate(top):
             label, cnt = item
-            s += '%i) "%s" %i\n' % (index+1,label, cnt)
+            s += '%i) "%s" %i\n' % (index+1, label, cnt)
         return s
-    
+
     def pprint_stats(self):
-        # endpoints
-        print self._pprint_topn(10,self.endpoints,"Top Endpoints for %s" % self.corename)
+        """Print statistics for a core
+        """
+        print self._pprint_topn(10, self.endpoints, "Top Endpoints for {0}".format(self.corename))
 
-        # search urls
-        print self._pprint_topn(10,self.urls,"Top Search URLs for %s" % self.corename)
+        print self._pprint_topn(10, self.urls, "Top Search URLs for {0}".format(self.corename))
 
-        # slowest calls
-        print self._pprint_topn(10,self.qtimes,"Slowest Searches for %s" % self.corename)
+        print self._pprint_topn(10, self.qtimes, "Slowest Searches for {0}".format(self.corename))
 
         # qtimes
-        print "Search Time for %s" % self.corename
-        print "="*40
+        print "Search Time for {0}\n{1}\n".format(self.corename, '='*40)
         stats = self.timestats()
         for st in stats:
             print "%s     %s" % (st[0], st[1])
-
         return ""
     
 class StatCounter(object):
@@ -85,7 +83,8 @@ class StatCounter(object):
             print cc.pprint_stats()
             print "*"* 100        
             print
-            
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
